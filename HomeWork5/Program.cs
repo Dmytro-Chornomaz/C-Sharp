b10cae5e602dac67fd094ee487d5bc38f6c7a58e
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-05-10 17:43:46 +0200

    Updated HomeWork5 to HW6

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,27 +1,86 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Drawing;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
-var file1 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\FIle1.txt", 5);
 
-var file2 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File2.txt", 7);
 
-var file3 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File3.txt", 12);
+//var file1 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\FIle1.txt", 5);
 
-var file4 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File4.txt", 3);
+//var file2 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File2.txt", 7);
 
-var file5 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File5.txt", 9);
+//var file3 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File3.txt", 12);
 
-await file1;
+//var file4 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File4.txt", 3);
 
-await file2;
+//var file5 = ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File5.txt", 9);
 
-await file3;
+//await file1;
 
-await file4;
+//await file2;
 
-await file5;
+//await file3;
+
+//await file4;
+
+//await file5;
+
+CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+
+CancellationToken token = cancelTokenSource.Token;
+
+Task task1 = new Task(() => ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\FIle1.txt", 5), token);
+Task task2 = new Task(() => ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File2.txt", 7), token);
+Task task3 = new Task(() => ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File3.txt", 12), token);
+Task task4 = new Task(() => ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File4.txt", 3), token);
+Task task5 = new Task(() => ReadAndPrintAsync(@"D:\\QA\\C#\\Hillel\\HomeWork5\\File5.txt", 9), token);
+
+List<Task> tasks = new List<Task>() { task1, task2, task3, task4, task5 };
+
+try
+{
+    foreach (var t in tasks)
+    {
+        t.Start();
+
+        if (t.Status == TaskStatus.RanToCompletion)
+        { 
+            cancelTokenSource.Cancel(); 
+        }
+        
+        t.Wait();
+        
+    }
+
+}
+
+catch (AggregateException theException)
+{
+    foreach (Exception e in theException.InnerExceptions)
+    {
+        if (e is TaskCanceledException)
+            Console.WriteLine("Operation aborted");
+        else
+            Console.WriteLine(e.Message);
+    }
+}
+
+Console.WriteLine($"Task Status: {task1.Status}");
+Console.WriteLine($"Task Status: {task2.Status}");
+Console.WriteLine($"Task Status: {task3.Status}");
+Console.WriteLine($"Task Status: {task4.Status}");
+Console.WriteLine($"Task Status: {task5.Status}");
+
+//-------------------------------------------------------
 
 async Task ReadAndPrintAsync(string path, int color)
 {
+    if (token.IsCancellationRequested)
+    { 
+        token.ThrowIfCancellationRequested();
+    }
+
     Console.ForegroundColor = (ConsoleColor)color;
 
     using (StreamReader reader = new StreamReader(path))
@@ -36,4 +95,9 @@ async Task ReadAndPrintAsync(string path, int color)
 
     Console.ResetColor();
 
+    
+
 }
+
+
+ 

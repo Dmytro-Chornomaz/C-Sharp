7697e117a35dc46a32823d5e4f0f09cf2268f42b
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-11-07 10:17:45 +0100

    Added XML_handler

---
+++ XML_handler/Parameters.cs
@@ -0,0 +1,14 @@
+﻿namespace XML_handler
+{
+    public class Parameters
+    {
+        public string tagName;
+        public string xPath;
+
+        public Parameters(string name, string path)
+        {
+            tagName = name;
+            xPath = path;
+        }
+    }
+}

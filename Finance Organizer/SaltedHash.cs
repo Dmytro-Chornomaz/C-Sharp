21db0a3227e038870181985ef1e6e5b97235901f
Dmytro-Chornomaz <bloody_justice@outlook.com>
2023-11-28 19:33:11 +0100

    Added SaltedHash

---
+++ Finance Organizer/SaltedHash.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Finance_Organizer
+{
+    public class SaltedHash
+    {
+        string salt = "Fr721*J3W_Sa";
+
+        public string ComputeSaltedHash(string password)
+        {
+            string passwordAndSalt = password + salt;
+
+            byte[] passwordAndSaltBytes = Encoding.UTF8.GetBytes(passwordAndSalt);
+
+            SHA256 hash = SHA256.Create();
+
+            byte[] hashBytes = hash.ComputeHash(passwordAndSaltBytes);
+
+            string hashValue = Convert.ToBase64String(hashBytes);
+
+            return hashValue;
+        }
+    }
+}
